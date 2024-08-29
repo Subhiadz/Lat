0f97846de8fe9043488251df567f985400433af6
Subhiadz <adzianiaja@gmail.com>
2024-08-29 09:09:43 +0700

    first commit

---
+++ latihan2.php
@@ -0,0 +1,32 @@
+<?php
+Class Database{
+    private $host, $username, $password, $db;
+    protected $conn;
+
+public function __construct(){
+    $this->host = 'localhost';
+    $this->username = 'root';
+    $this->password = '';
+    $this->db = 'bimbingan';
+    
+    $this->conn = new mysqli($this->host, $this->username, $this->password, $this->db) ;
+ }
+}
+
+class Student extends Database{
+   
+    public function dataView(){
+       $doc =  $this->conn->query("SELECT * FROM students") or die($this->conn->error);
+       return $doc;
+    }
+ 
+    public function student($id){
+       $data = $this->conn->query("SELECT * FROM `students` WHERE `id_student` = $id");
+       $row = mysqli_fetch_array($data);
+       return $row['name'];
+    }
+}    
+$student = new Student();
+$data_student = $student->dataView();
+
+?>
